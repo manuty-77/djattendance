0c7e21f4708f5bd2027de72dd024b9e3cd36b8ab
Kevin Sung <ksung@Kevins-MacBook-Air.local>
2018-03-09 18:09:44 -0800

    update reports module

--- ap/reports/forms.py
+++ ap/reports/forms.py
@@ -3,15 +3,18 @@ from django import forms
 
 #from accounts.models import Trainee
 #from accounts.widgets import TraineeSelect2MultipleInput
-
+from aputils.widgets import DatePicker
 import datetime
 
 
 class ReportGenerateForm(forms.Form):
   #training_class = ModelChoiceField(Class.objects.all(), empty_label=None)
+  date_from = forms.DateField(widget=DatePicker(), label="From: ")
+  date_to = forms.DateField(widget=DatePicker(), label="to: ")
   attendancePeriodStart = forms.DateTimeField(initial=datetime.date.today)
   attendancePeriodEnd = forms.DateTimeField(initial=datetime.date.today)
   Gender = forms.CharField(initial='M')
   Term = forms.IntegerField(initial='1')
-  Category = forms.CharField(max_length=500)
+  GeneralItems = forms.CharField(max_length=500)
+  GeneralReport = forms.CharField(max_length=500)
   
--- ap/reports/views.py
+++ ap/reports/views.py
@@ -5,9 +5,13 @@ from django.views.generic.list import ListView
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 from .forms import ReportGenerateForm
-import datetime
+from datetime import date, timedelta, datetime
 from django.utils import timezone
 
+from accounts.models import Trainee
+from terms.models import Term
+from leaveslips.models import IndividualSlip, GroupSlip
+from attendance.models import Roll
 
 class ReportCreateView(LoginRequiredMixin, GroupRequiredMixin, FormView):
   template_name = 'reports/reports.html'
@@ -16,14 +20,144 @@ class ReportCreateView(LoginRequiredMixin, GroupRequiredMixin, FormView):
   success_url = reverse_lazy('reports:generate-reports')
   form_class = ReportGenerateForm
 
+  def is_excused(trainee, roll):
+    '''
+    Checks if a roll in where the trainee is absent or tardy has been excused or not.
+    '''
+    individual_slips = IndividualSlip.objects.filter(trainee=trainee, rolls=roll)
+    if len(individual_slips) > 0:
+      return individual_slips[0].status == 'A' or individual_slips[0].status == 'S'
+    
+    #get group slips related to the trainee and event of the roll in question, and make sure the group_slip is for the right date
+    group_slips = GroupSlip.objects.filter(trainees=trainee, events=roll.event, start__range=[roll.date, roll.date+timedelta(days=1)])
+
+
+    return False
+
   def post(self, request, *args, **kwargs):
-    data = request.POST
-    print "Checking data..."
-    print data
-    print "checking request..."
-    print request
+    data = dict(request.POST.iterlists())
+    rtn_data = dict() #{TRAINEE_NAME: {Absences - Total: 10, Tardies - Total: 5, ...}, TRAINEE_NAME: ...}
+
+    date_from = datetime.strptime(data['date_range'][0].split(';')[0], '%m/%d/%Y')
+    date_to = datetime.strptime(data['date_range'][0].split(';')[1], '%m/%d/%Y')
+    delta = date_to - date_from
+
+    date_list = []
+    for i in range(delta.days + 1):
+      date_list.append(date_from + timedelta(days=i))
+
+    trainees = Trainee.objects.all()
+
+    gender = data['gender'][0].split(';')
+
+    if "Male" not in gender:
+      trainees = trainees.filter(gender="S")
+    elif "Female" not in gender:
+      trainees = trainees.filter(gender="B")
+    elif "Male" not in gender and "Female" not in gender:
+      trainees = Trainee.objects.none()
+      return rtn_data
+
+    #Return trainees only for the terms requested by report
+    terms_filter = [int(s) for s in data['term'][0] if s.isdigit()]
+    filtered_trainees = Trainee.objects.none()
+    for term in terms_filter:
+      filtered_trainees = filtered_trainees | trainees.filter(current_term=int(term))
+
+    filtered_rolls = Roll.objects.filter(trainee__in=filtered_trainees, date__in=date_list)
+    print "*************** FILTERED ROLLS ***************"
+    print str(filtered_rolls)
+
+    items_for_query = data['general_report'][0].split(';')
+
+    #IndividualSlip.objects.all().filter(rolls=Roll.objects.all()[20])
+
+    #'Present'
+    #'Absent'
+    #'Tardy'
+    #'Uniform'
+    #'Left Class'
+
+
+    for roll in filtered_rolls:
+      #print "roll date: " + str(roll.date)
+      if roll.trainee.full_name not in rtn_data:
+        rtn_data[roll.trainee.full_name] = {}
+      if roll.status == 'P':
+        if not 'Present' in rtn_data[roll.trainee.full_name]:
+          rtn_data[roll.trainee.full_name]['Present'] = 1
+        else:
+          rtn_data[roll.trainee.full_name]['Present'] += 1
+      #logic for each 'elif' is check if item for report is present in dictionary...
+      #if not, create the item for report for the trainee
+      #but check to make sure it's an item to report
+      elif roll.status == 'A':
+        if not 'Absences - Total' in rtn_data[roll.trainee.full_name] and 'Absences - Total' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Absences - Total'] = 1
+        elif 'Absences - Total' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Absences - Total'] += 1
+      #for tardy rolls add to total tardy count as well as type of tardy count
+      elif roll.status == 'T':
+        if not 'Tardies - Total' in rtn_data[roll.trainee.full_name] and 'Tardies - Total' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Total'] = 1
+        elif 'Tardies - Total' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Total'] += 1
+        if not 'Tardies - Late' in rtn_data[roll.trainee.full_name] and 'Tardies - Late' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Late'] = 1
+        elif 'Tardies - Late' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Late'] += 1
+      elif roll.status == 'U':
+        if not 'Tardies - Total' in rtn_data[roll.trainee.full_name] and 'Tardies - Total' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Total'] = 1
+        elif 'Tardies - Total' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Total'] += 1
+        if not 'Tardies - Uniform' in rtn_data[roll.trainee.full_name] and 'Tardies - Uniform' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Uniform'] = 1
+        elif 'Tardies - Uniform' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Uniform'] += 1
+      elif roll.status == 'L':
+        if not 'Tardies - Total' in rtn_data[roll.trainee.full_name] and 'Tardies - Total' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Total'] = 1
+        elif 'Tardies - Total' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Total'] += 1
+        if not 'Tardies - Left Class' in rtn_data[roll.trainee.full_name] and 'Tardies - Left Class' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Left Class'] = 1
+        elif 'Tardies - Left Class' in items_for_query:
+          rtn_data[roll.trainee.full_name]['Tardies - Left Class'] += 1
+
+    """
+    for item in items_for_query:
+      if item == "Absences - Excused":
+      elif item == "Absences - Unexcused":
+      elif item == "Absences - Total":
+      elif item == "Absences - Unexcused and Sickness":
+      elif item == "Tardies - Late":
+      elif item == "Tardies - Left Class":
+      elif item == "Tardies - Uniform":
+      elif item == "Tardies - Total":
+      elif item == "Number of LS":
+      elif item == "Classes Missed":
+    """
+
+
+    #for t in filtered_trainees:
+    #  absences = 0
+    #  tardies = 0
+    #  for each_roll in t.get_attendance_record():
+    #    if each_roll['attendance'] == 'A':
+    #      absences += 1
+    #    elif each_roll['attendance'] == 'T':
+    #      tardies += 1
+    #  rtn_data[t.get_full_name()] = {'Total Absences': absences, 'Total Tardies': tardies}
+    
+    print rtn_data
+
+
     #context = super(ReportCreateView, self).get_context_data(**kwargs)
-    return render(request, 'reports/reports.html', {'data': data})
+
+    #based on data, filter all the relevant data for the report
+
+    return render(request, 'reports/reports.html', {'data': rtn_data})
 
 
 class GeneratedReport(LoginRequiredMixin, GroupRequiredMixin, ListView):
