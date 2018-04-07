6247dd2df28d25f3b624abe277217ad6faaf6e51
iamsitting <cdsalamanca27@gmail.com>
2018-04-07 11:38:16 -0700

    new output method

--- ap/attendance/management/commands/data_check_rolls.py
+++ ap/attendance/management/commands/data_check_rolls.py
@@ -7,53 +7,58 @@ from datetime import datetime
 from leaveslips.models import IndividualSlip
 
 
+def out(s, f):
+  print s
+  f.write(s)
+
+
 class Command(BaseCommand):
   # to use: python ap/manage.py data_check_rolls --settings=ap.settings.dev
 
   def _mislink_rolls(self):
     # Pulls all rolls that has a mislink, the event that the rolls points to does not exist in the trainee's active schedule
     right_now = datetime.now().strftime("%m%d%Y_%H%M%S")
+    rolls = Roll.objects.all().order_by('event__id', 'date')
+    ct = Term.current_term()
+    output = '{0}: {1}-- Submitted by: {2} \n'
     with open('../mislink_rolls' + right_now + '.txt', 'w') as f:
-      rolls = Roll.objects.all().order_by('event__id', 'date')
-      ct = Term.current_term()
-      output = '{0}: {1}-- Submitted by: {2} \n'
       for r in rolls:
         try:
           schedules = r.event.schedules.all()
           for s in schedules:
             if r.trainee.id not in s.trainees.values_list('id', flat=True):
-              print 'Trainee DNM:'
-              print output.format(str(r.id), r, r.submitted_by)
-              f.write('Trainee DNM: ')
-              f.write(output.format(str(r.id), r, r.submitted_by))
+              out('Trainee DNM: ', f)
+              out(output.format(str(r.id), r, r.submitted_by), f)
               break
             else:
               if str(ct.term_week_of_date(r.date)) not in s.weeks:
-                print 'Roll on wrong week'
-                print output.format(str(r.id), r, r.submitted_by)
-                f.write('Wrong Week: ')
-                f.write(output.format(str(r.id), r, r.submitted_by))
+                out('Wrong Week: ', f)
+                out(output.format(str(r.id), r, r.submitted_by), f)
                 break
         except Exception as e:
-          print output.format(str(r.id), e, r.submitted_by)
-          f.write(output.format(str(r.id), e, r.submitted_by))
+          out(output.format(str(r.id), e, r.submitted_by), f)
 
   def _ghost_rolls(self):
     # Pull all rolls that have a present status with no leavslips attached
     right_now = datetime.now().strftime("%m%d%Y_%H%M%S")
-    f = open('../ghost_rolls' + right_now + '.txt', 'w')
-    output = '{0}: {1}-- Submitted by: {2} \n'
     rolls = Roll.objects.filter(status='P').order_by('date')
-    for r in rolls:
-      try:
-        slips = r.leaveslips.all()
-        if slips.count() == 0:
-          print output.format(r.id, r, r.submitted_by)
-          f.write(output.format(r.id, r, r.submitted_by))
-      except Exception as e:
-        print output.format(r.id, e, r.submitted_by)
-        f.write(output.format(r.id, e, r.submitted_by))
-    f.close()
+    output = '{0}: {1}-- Submitted by: {2} \n'
+    output2 = 'For {0}: Possible Slip: {1} [ID: {2}]\n'
+
+    def find_possible_slips(roll):
+      # check to see if there's a leaveslip submitted by the trainee for other rolls or events on the date that this roll takes place
+      return roll.trainee.individualslips.filter(rolls__date__in=[roll.date])
+
+    with open('../ghost_rolls' + right_now + '.txt', 'w') as f:
+      for r in rolls:
+        try:
+          slips = r.leaveslips.all()
+          if slips.count() == 0:
+            out(output.format(r.id, r, r.submitted_by), f)
+            for s in find_possible_slips(r):
+              out(output2.format(r.id, s, s.id), f)
+        except Exception as e:
+          out(output.format(r.id, e, r.submitted_by), f)
 
   def _mislink_leaveslips(self):
     # Pull all leaveslips submitted by trainee X and has rolls not for trainee X
@@ -74,8 +79,8 @@ class Command(BaseCommand):
 
   def handle(self, *args, **options):
     print('* Pulling Rolls with mislinked Trainee...')
-    self._mislink_rolls()
+    #self._mislink_rolls()
     print('* Pulling "present" Rolls with no leavslips attached...')
-    #self._ghost_rolls()
+    self._ghost_rolls()
     print('* Pulling leaveslips with rolls that do not belong to submitting trainee')
     #self._mislink_leaveslips()
